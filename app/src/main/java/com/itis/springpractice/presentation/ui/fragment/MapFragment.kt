ee5189a1c3f08c5e84d27d00a47b555afaa76cb3
Ildar Muhaev <ildar.muckhaev@gmail.com>
2022-05-08 05:01:37 +0400

    location layer

--- app/src/main/java/com/itis/springpractice/presentation/ui/fragment/MapFragment.kt
+++ app/src/main/java/com/itis/springpractice/presentation/ui/fragment/MapFragment.kt
@@ -1,22 +1,21 @@
 package com.itis.springpractice.presentation.ui.fragment
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.SharedPreferences
-import android.location.Location
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.graphics.PointF
 import android.os.Bundle
-import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import com.google.android.gms.location.*
 import com.google.android.material.snackbar.Snackbar
-import com.itis.springpractice.BuildConfig
 import com.itis.springpractice.R
 import com.itis.springpractice.databinding.FragmentMapBinding
 import com.itis.springpractice.di.UserAuthContainer
@@ -28,19 +27,36 @@ import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.StyleType
 import com.yandex.mapkit.geometry.Point
 import com.yandex.mapkit.layers.GeoObjectTapListener
-import com.yandex.mapkit.map.CameraPosition
-import com.yandex.mapkit.map.GeoObjectSelectionMetadata
+import com.yandex.mapkit.layers.ObjectEvent
+import com.yandex.mapkit.logo.Alignment
+import com.yandex.mapkit.logo.HorizontalAlignment
+import com.yandex.mapkit.logo.VerticalAlignment
+import com.yandex.mapkit.map.*
 import com.yandex.mapkit.map.Map
-import com.yandex.mapkit.map.MapType
 import com.yandex.mapkit.mapview.MapView
+import com.yandex.mapkit.user_location.UserLocationLayer
+import com.yandex.mapkit.user_location.UserLocationObjectListener
+import com.yandex.mapkit.user_location.UserLocationView
+import com.yandex.runtime.image.ImageProvider
 
-class MapFragment : Fragment() {
+
+class MapFragment : Fragment(), UserLocationObjectListener, CameraListener {
     private lateinit var binding: FragmentMapBinding
     private lateinit var mapViewModel: MapViewModel
     private lateinit var mapView: MapView
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var userLocationLayer: UserLocationLayer
     private lateinit var sharedPreferences: SharedPreferences
     private lateinit var mapCity: Map
+
+    private var defaultLocation = Point(0.0, 0.0)
+    private var routeStartLocation: Point? = defaultLocation
+
+    private var permissionLocation = false
+    private var followUserLocation = false
+
+    private val mapKit by lazy {
+        MapKitFactory.getInstance()
+    }
 
     override fun onStart() {
         super.onStart()
@@ -70,11 +86,25 @@ class MapFragment : Fragment() {
             mapViewModel.onDeleteTokenClick()
             findNavController().navigate(R.id.action_mapFragment_to_signInFragment)
         }
-
-        getLocationPermissions()
         mapView = binding.mapCity
         mapInit()
-        initObservers()
+        checkPermission()
+        userInterface()
+    }
+
+    private fun checkPermission() {
+        val permissionLocation =
+            context?.let {
+                ContextCompat.checkSelfPermission(
+                    it,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            }
+        if (permissionLocation != PackageManager.PERMISSION_GRANTED) {
+            getLocationPermissions()
+        } else {
+            userLocationConfig()
+        }
     }
 
     private fun mapInit() {
@@ -82,16 +112,32 @@ class MapFragment : Fragment() {
         mapCity.mapType = MapType.VECTOR_MAP
         mapCity.setMapStyle(StyleType.V_MAP2.toString())
         mapCity.addTapListener(mapObjectTapListener)
+        mapCity.isZoomGesturesEnabled = true
+        mapCity.isRotateGesturesEnabled = true
+        mapCity.isFastTapEnabled = true
+        mapCity.isIndoorEnabled = true
     }
 
-    private fun modifyMap(location: Location) {
-        mapCity.move(
-            CameraPosition(Point(location.latitude, location.longitude), 16.0f, 0.0f, 0.0f),
-            Animation(
-                Animation.Type.SMOOTH, 2F
-            ),
-            null
-        )
+    private fun userLocationConfig() {
+        userLocationLayer = mapKit.createUserLocationLayer(mapView.mapWindow)
+        userLocationLayer.isVisible = true
+        userLocationLayer.isHeadingEnabled = true
+        userLocationLayer.setObjectListener(this)
+        mapCity.addCameraListener(this)
+        cameraUserPosition()
+        permissionLocation = true
+    }
+
+    private fun modifyMap(location: Point?) {
+        if (location != null) {
+            mapCity.move(
+                CameraPosition(Point(location.latitude, location.longitude), 18.0f, 0.0f, 0.0f),
+                Animation(
+                    Animation.Type.SMOOTH, 1F
+                ),
+                null
+            )
+        }
     }
 
     private fun getLocationPermissions() {
@@ -103,49 +149,84 @@ class MapFragment : Fragment() {
         )
     }
 
-    @SuppressLint("MissingPermission")
+    private fun userInterface() {
+        val mapLogoAlignment = Alignment(HorizontalAlignment.LEFT, VerticalAlignment.BOTTOM)
+        mapCity.logo.setAlignment(mapLogoAlignment)
+        binding.userLocationFab.setOnClickListener {
+            if (permissionLocation) {
+                cameraUserPosition()
+                followUserLocation = true
+            } else {
+                checkPermission()
+            }
+        }
+    }
+
+    private fun cameraUserPosition() {
+        if (userLocationLayer.cameraPosition() != null) {
+            routeStartLocation = userLocationLayer.cameraPosition()?.target
+            modifyMap(routeStartLocation)
+        } else {
+            modifyMap(defaultLocation)
+        }
+    }
+
     private val locationPermissionRequest = registerForActivityResult(
         ActivityResultContracts.RequestMultiplePermissions()
     ) { permissions ->
         when {
             permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
-                // Precise location access granted so start GPS updates
-                val locationRequest: LocationRequest = LocationRequest.create().apply {
-                    interval = 1000
-                    fastestInterval = 500
-                    priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-                }
-
-                val locationCallback = object : LocationCallback() {
-                    override fun onLocationResult(locationResult: LocationResult) {
-                        super.onLocationResult(locationResult)
-                        for (locationSuccess in locationResult.locations) {
-                            // TODO
-                        }
-                    }
-                }
-                fusedLocationClient =
-                    LocationServices.getFusedLocationProviderClient(requireActivity())
-                fusedLocationClient.lastLocation.addOnSuccessListener {
-                    mapViewModel.onPermissionResult(it)
-                }
-                fusedLocationClient.requestLocationUpdates(
-                    locationRequest,
-                    locationCallback,
-                    Looper.getMainLooper()
-                )
+                userLocationConfig()
             }
             permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
-                val fusedLocationClient =
-                    LocationServices.getFusedLocationProviderClient(requireActivity())
-                fusedLocationClient.lastLocation.addOnSuccessListener {
-                    mapViewModel.onPermissionResult(it)
-                }
+                userLocationConfig()
             }
             else -> {
                 showMessage("Скоро будет доступен поиск мест")
             }
         }
+    }
+
+    override fun onObjectAdded(userLocationView: UserLocationView) {
+        setAnchor()
+        userLocationView.pin.setIcon(ImageProvider.fromResource(context, R.drawable.arrow))
+        userLocationView.arrow.setIcon(ImageProvider.fromResource(context, R.drawable.arrow))
+        userLocationView.accuracyCircle.fillColor = Color.LTGRAY and -0x66000001
+    }
+
+    override fun onObjectRemoved(userView: UserLocationView) {}
+
+    override fun onObjectUpdated(userView: UserLocationView, objEvent: ObjectEvent) {}
+
+    override fun onCameraPositionChanged(
+        map: Map,
+        cameraPosition: CameraPosition,
+        cameraUpdateReason: CameraUpdateReason,
+        finish: Boolean
+    ) {
+        if (finish) {
+            if (followUserLocation) {
+                setAnchor()
+            }
+        } else {
+            if (!followUserLocation) {
+                noAnchor()
+            }
+        }
+    }
+
+    private fun setAnchor() {
+        userLocationLayer.setAnchor(
+            PointF((mapView.width * 0.5).toFloat(), (mapView.height * 0.5).toFloat()),
+            PointF((mapView.width * 0.5).toFloat(), (mapView.height * 0.83).toFloat())
+        )
+        binding.userLocationFab.setImageResource(R.drawable.ic_my_location_black)
+        followUserLocation = false
+    }
+
+    private fun noAnchor() {
+        userLocationLayer.resetAnchor()
+        binding.userLocationFab.setImageResource(R.drawable.ic_location_searching_black)
     }
 
     private fun showMessage(message: String) {
@@ -154,12 +235,6 @@ class MapFragment : Fragment() {
             message,
             Snackbar.LENGTH_LONG
         ).show()
-    }
-
-    private fun initObservers() {
-        mapViewModel.location.observe(viewLifecycleOwner) {
-            modifyMap(it)
-        }
     }
 
     private fun initObjects() {
@@ -188,3 +263,4 @@ class MapFragment : Fragment() {
         MapKitFactory.getInstance().onStop()
     }
 }
+
